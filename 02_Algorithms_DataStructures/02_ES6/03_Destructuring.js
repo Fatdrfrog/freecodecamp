3619b49bd22e16d2a54101f9f8fa7dd403b44bc1
Samat Kalshabekov <fatdrfrog@gmail.com>
2023-11-26 21:24:19 +0600

    Submit Destructuring 01

---
+++ 02_Algorithms_DataStructures/02_ES6/03_Destructuring.js
@@ -0,0 +1,12 @@
+// Extract Values from Objects
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80,
+};
+const { today, tomorrow } = HIGH_TEMPERATURES;
+
+// Assign Variables from Objects
+  const {today:highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+
+// Assign Variables from Nested Objects
