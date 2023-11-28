a55e59440faec04fdaa1ce0f083283dc45b7f36d
Samat Kalshabekov <fatdrfrog@gmail.com>
2023-11-28 22:33:07 +0600

    Submit OOP

--- 02_Algorithms_DataStructures/02_ES6/05_OOP_Beginning.js
+++ 02_Algorithms_DataStructures/02_ES6/05_OOP_Beginning.js
@@ -8,3 +8,21 @@ class Vegetable{
   console.log(carrot.name); 
 
   // Use getters and setters to Control Access to an Object
+  class Thermostat{
+  constructor(temp){
+    this._temp = temp;
+  }
+
+  get temperature(){
+    return 5/9 * (this._temp - 32);
+  }
+
+  set temperature(c){
+    this._temp = c * 9/5 + 32;
+  }
+}
+
+const thermos = new Thermostat(76);
+let temp = thermos.temperature;
+thermos.temperature = 26;
+temp = thermos.temperature;
