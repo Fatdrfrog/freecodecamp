dffd91037eeb11ec1a196655d7426cd0d16fb9a3
Samat Kalshabekov <fatdrfrog@gmail.com>
2023-11-30 14:46:31 +0600

    Submit Modular JS final

--- 02_Algorithms_DataStructures/02_ES6/06_Modular_JS/index.js
+++ 02_Algorithms_DataStructures/02_ES6/06_Modular_JS/index.js
@@ -1,4 +1,12 @@
 import { uppercaseString, lowercaseString } from './string_functions.js';
+import * as stringFunctions from "./string_functions.js";
+import subtract from './math_functions.js'  
+
+subtract(7,4);
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
 
 uppercaseString("hello");
 lowercaseString("WORLD!");
+
+
