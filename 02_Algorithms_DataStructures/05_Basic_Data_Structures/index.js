d04b09750ab98a3d2abb510910ff9169c1890361
Samat Kalshabekov <fatdrfrog@gmail.com>
2023-12-04 23:56:05 +0600

    Submit commit

--- 02_Algorithms_DataStructures/05_Basic_Data_Structures/index.js
+++ 02_Algorithms_DataStructures/05_Basic_Data_Structures/index.js
@@ -24,3 +24,49 @@ function popShift(arr) {
   console.log(popShift(['challenge', 'is', 'not', 'complete']));
 
 // Remove Items Using splice()
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+arr.splice(1,4)
+console.log(arr);
+
+// Add Items Using splice()
+function htmlColorNames(arr) {
+  arr.splice(0,2,'DarkSalmon','BlanchedAlmond')
+  return arr;
+}
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+
+// Copy Array Items Using slice()
+function forecast(arr) {
+  return arr.slice(2,4);
+}
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+// Copy an Array with the Spread Operator
+function copyMachine(arr, num) {
+  let newArr = [];
+  while (num >= 1) {
+    newArr.push([...arr]);
+    num--;
+  }
+  return newArr;
+}
+
+console.log(copyMachine([true, false, true], 2));
+
+// Combine Arrays with the Spread Operator
+function spreadOut() {
+  let fragment = ['to', 'code'];
+  let sentence=['learning',...fragment,'is','fun'];
+  return sentence;
+}
+console.log(spreadOut());
+
+// Check For The Presence of an Element With indexOf()
+function quickCheck(arr, elem) {
+  if(arr.indexOf(elem)!==-1) return true;
+  return false;
+}
+console.log(quickCheck(["onions", "squash", "shallots"], "onions"));
+
+// Iterate Through All an Array's Items Using For Loops
+console.log("Commiting just for commit")
